74e973ae9c9078667a7588d5297a61ededf7957e
Vojta Jina <vojta.jina@gmail.com>
2012-09-30 23:29:14 -0700

    Add CLI equivalents into example config

--- test/client/testacular.conf.js
+++ test/client/testacular.conf.js
@@ -25,22 +25,28 @@ exclude = [];
 
 // use dots reporter, as travis terminal does not support escaping sequences
 // possible values: 'dots', 'progress', 'junit'
+// CLI --reporters progress
 reporters = ['progress'];
 
 // web server port
+// CLI --port 9876
 port = 9876;
 
 // cli runner port
+// CLI --runner-port 9100
 runnerPort = 9100;
 
 // enable / disable colors in the output (reporters and logs)
+// CLI --colors --no-colors
 colors = true;
 
 // level of logging
 // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+// CLI --log-level debug
 logLevel = LOG_INFO;
 
 // enable / disable watching file and executing tests whenever any file changes
+// CLI --auto-watch --no-auto-watch
 autoWatch = true;
 
 // Start these browsers, currently available:
@@ -51,15 +57,19 @@ autoWatch = true;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
+// CLI --browsers Chrome,Firefox,Safari
 browsers = [];
 
 // If browser does not capture in given timeout [ms], kill it
+// CLI --capture-timeout 5000
 captureTimeout = 5000;
 
 // Auto run tests on start (when browsers are captured) and exit
+// CLI --single-run --no-single-run
 singleRun = false;
 
 // report which specs are slower than 500ms
+// CLI --report-slower-than 500
 reportSlowerThan = 500;
 
 // compile coffee scripts
