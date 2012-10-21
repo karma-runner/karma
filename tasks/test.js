53c0ddfbca62c212c2245184c3344ee199ff3e23
Friedel Ziegelmayer <friedel.ziegelmayer@gmail.com>
2012-10-21 19:00:47 +0200

    Update grunt to v0.4.0a:  * Update custom tasks to new grunt conventions (utils -> util; no helpers; relative paths in spawn)  * fix grunt-jasmine-node so that it points to Dignifiedquires fork

--- tasks/test.js
+++ tasks/test.js
@@ -15,7 +15,7 @@ module.exports = function(grunt) {
     var which = require('which').sync;
 
     var exec = function(cmd, args, failMsg) {
-      var child = grunt.utils.spawn({cmd: cmd, args: args}, function(err, result, code) {
+      var child = grunt.util.spawn({cmd: cmd, args: args}, function(err, result, code) {
         if (code) {
           console.error(err);
           grunt.fail.fatal(failMsg, code);
@@ -33,7 +33,7 @@ module.exports = function(grunt) {
     if (this.target === 'e2e') {
       var tests = grunt.file.expand(this.data);
       var processToKill;
-      var cmd = './bin/testacular';
+      var cmd = '../bin/testacular';
       var node = which('node');
       var args = [
         'start', null, '--single-run', '--no-auto-watch', '--reporters=dots', '--browsers=' + BROWSERS
@@ -45,13 +45,13 @@ module.exports = function(grunt) {
         }
 
         if (err || code) {
-          // console.error(err);
+          console.error(err);
           grunt.fail.fatal('E2E test "' + args[1] + '" failed.', code);
         } else {
           args[1] = tests.shift();
           if (args[1]) {
             if (args[1] === 'test/e2e/angular-scenario/testacular.conf.js') {
-              processToKill = grunt.utils.spawn({
+              processToKill = grunt.util.spawn({
                 cmd: node, args: ['test/e2e/angular-scenario/server.js']
               }, function() {});
             }
@@ -59,9 +59,9 @@ module.exports = function(grunt) {
             grunt.log.writeln('Running ' + cmd + args.join(' '));
             var child;
             if (process.platform === 'win32') {
-              child = grunt.utils.spawn({cmd: node, args: [cmd].concat(args)}, next);
+              child = grunt.util.spawn({cmd: node, args: [cmd].concat(args)}, next);
             } else {
-              child = grunt.utils.spawn({cmd: cmd, args: args}, next);
+              child = grunt.util.spawn({cmd: cmd, args: args}, next);
             }
 
             child.stdout.pipe(process.stdout);
