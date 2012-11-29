3643eac83488376f1a6a865e737182fe77f3599c
Fred Sauer <fredsa@google.com>
2012-11-29 14:51:35 -0800

    [test] Update test/e2e/angular-scenario/e2eSpec.js to fix sample e2e test.

--- test/e2e/angular-scenario/e2eSpec.js
+++ test/e2e/angular-scenario/e2eSpec.js
@@ -1,15 +1,14 @@
-
 /** A Sample Angular E2E test */
 
 describe('My Sample App', function() {
 
-  iit('should let Angular do its work', function() {
+  it('should let Angular do its work', function() {
     browser().navigateTo('/index.html');
     input('yourName').enter('A Pirate!');
     expect(element('.ng-binding').text()).toEqual('Hello A Pirate!!');
   });
 
-  it('should skip this e2e test', function() {
+  xit('should skip this e2e test', function() {
     sleep(15);
     browser().navigateTo('/index.html');
   });
