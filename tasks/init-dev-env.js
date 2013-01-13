f629e299c11590da776e4c1f2025f9d5ada328ad
Vojta Jina <vojta.jina@gmail.com>
2013-01-13 15:06:29 -0800

    chore: add "init-dev-env" grunt task
    To make it easier for people to initialize the development environment.

---
+++ tasks/init-dev-env.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+
+  /**
+   * Initialize development environment for Testacular
+   *
+   * - register git hooks (commit-msg)
+   */
+  grunt.registerTask('init-dev-env', 'Initialize dev environment.', function() {
+    var fs = require('fs');
+    var done = this.async();
+
+    fs.symlink('../../tasks/lib/validate-commit-msg.js', '.git/hooks/commit-msg', function(e) {
+      if (!e) {
+        grunt.log.ok('Hook "validate-commit-msg" installed.');
+      }
+      done();
+    });
+  });
+};
