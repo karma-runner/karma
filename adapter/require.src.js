59dbdbd136baa87467b9b9a4cb6ce226ae87bbef
Vojta Jina <vojta.jina@gmail.com>
2013-02-02 14:03:37 -0800

    fix(adapter.requirejs): show error if no timestamp defined for a file
    And do not append the timestamp at all, maybe it's being loaded from a proxy.

--- adapter/require.src.js
+++ adapter/require.src.js
@@ -4,7 +4,13 @@
 
 var load_original = window.requirejs.load;
 requirejs.load = function (context, moduleName, url) {
-  return load_original.call(this, context, moduleName, url + '?' + __testacular__.files[url]);
+  if (__testacular__.files.hasOwnProperty(url)) {
+    url = url + '?' + __testacular__.files[url];
+  } else {
+    console.error('There is no timestamp for ' + url + '!');
+  }
+
+  return load_original.call(this, context, moduleName, url);
 };
 
 require.config({
