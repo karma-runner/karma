c1093826413067038c1f6ac250822cbef7d8aca9
Vojta Jina <vojta.jina@gmail.com>
2013-02-18 20:19:01 -0800

    test(e2e): fix the coverageRequire e2e test
    Broken by 63f3f409ae85a5137396a7ed6537bedfe4437cb3, now we need to set the baseUrl manually.

--- test/e2e/coverageRequirejs/main.js
+++ test/e2e/coverageRequirejs/main.js
@@ -1,3 +1,10 @@
-require(['test'], function() {
-  window.__testacular__.start();
+require.config({
+  // Testacular serves files under /base, which is the basePath from your config file
+  baseUrl: '/base',
+
+  // load test.js
+  deps: ['test'],
+
+  // we have to kick of jasmine, as it is asynchronous
+  callback: window.__testacular__.start
 });
