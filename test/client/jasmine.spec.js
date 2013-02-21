435bf72cb12112462940c8114fbaa19f9de38531
Vojta Jina <vojta.jina@gmail.com>
2013-02-20 19:12:48 -0800

    feat(adapter.jasmine): remove only last failed specs anti-feature
    This really seems to be anti-feature, as it only causes troubles.
    
    Closes #148

--- test/client/jasmine.spec.js
+++ test/client/jasmine.spec.js
@@ -46,26 +46,6 @@ describe('jasmine adapter', function() {
     });
 
 
-    it('should report failed ids', function() {
-      var specs = [                                  // id
-        spec,                                        // 0
-        new jasmine.Spec(env, suite, 'should test'), // 1
-        new jasmine.Spec(env, suite, 'should test'), // 2
-        new jasmine.Spec(env, suite, 'should test')  // 3
-      ];
-
-      specs[1].fail(new Error('Some error'));
-      specs[2].fail(new Error('Another error'));
-
-      while(specs.length) {
-        reporter.reportSpecResults(specs.shift());
-      }
-      reporter.reportRunnerResults();
-
-      expect(testacular.store('jasmine.lastFailedIds')).toEqual([1, 2]);
-    });
-
-
     it('should remove jasmine-specific frames from the exception stack traces', function() {
       var error = new Error("Expected 'function' to be 'fxunction'");
       error.stack = "Error: Expected 'function' to be 'fxunction'.\n" +
@@ -154,8 +134,6 @@ describe('jasmine adapter', function() {
 
     beforeEach(function() {
       tc = new Testacular(new MockSocket(), {});
-      tc.store('jasmine.lastFailedIds', [1, 3, 5]);
-      tc.store('jasmine.lastCount', 10);
 
       spyOn(tc, 'info');
       spyOn(tc, 'complete');
@@ -163,93 +141,6 @@ describe('jasmine adapter', function() {
 
       jasmineEnv = new jasmine.Env();
       start = createStartFn(tc, jasmineEnv);
-    });
-
-
-    it('should reset last results', function() {
-      start();
-      expect(tc.store('jasmine.lastCount')).toBe(0);
-      expect(tc.store('jasmine.lastFailedIds')).toEqual([]);
-    });
-
-
-    it('should store failed ids', function() {
-      jasmineEnv.describe('fake', function() {
-        jasmineEnv.it('should pass', function() {});
-        jasmineEnv.it('should fail', function() {throw new Error('FAIL');});
-      });
-
-      start();
-
-      waitsFor(function() {
-        return tc.store('jasmine.lastFailedIds').length === 1;
-      }, 'execution finish', 50);
-
-      runs(function() {
-        expect(tc.store('jasmine.lastFailedIds')).toEqual([1]);
-      });
-    });
-
-
-    describe('specFilter', function() {
-      var originalSpecFilter = 'ORIGINAL SPEC FILTER';
-
-      beforeEach(function() {
-        jasmineEnv.specFilter = originalSpecFilter;
-      });
-
-
-      it('should filter only last failed', function() {
-        tc.store('jasmine.lastFailedIds', [1, 3, 5]);
-        tc.store('jasmine.lastCount', 5);
-        jasmineEnv.nextSpecId_ = 5;
-
-        start();
-        expect(jasmineEnv.specFilter({id: 1})).toBe(true);
-        expect(jasmineEnv.specFilter({id: 2})).toBe(false);
-        expect(jasmineEnv.specFilter({id: 3})).toBe(true);
-        expect(jasmineEnv.specFilter({id: 4})).toBe(false);
-        expect(jasmineEnv.specFilter({id: 5})).toBe(true);
-      });
-
-
-      it('should not filter if first run', function() {
-        tc.store('jasmine.lastFailedIds', null);
-        tc.store('jasmine.lastCount', null);
-
-        start();
-        expect(jasmineEnv.specFilter).toBe(originalSpecFilter);
-      });
-
-
-      it('should not filter if number of specs changed', function() {
-        tc.store('jasmine.lastCount', 10);
-        jasmineEnv.nextSpecId_ = 5;
-
-        start();
-        expect(jasmineEnv.specFilter).toBe(originalSpecFilter);
-      });
-
-
-      it('should not filter if all specs passed last time', function() {
-        tc.store('jasmine.lastFailedIds', []);
-        tc.store('jasmine.lastCount', 5);
-        jasmineEnv.nextSpecId_ = 5;
-
-        start();
-        expect(jasmineEnv.specFilter).toBe(originalSpecFilter);
-      });
-
-
-      it('should not filter if exclusive mode', function() {
-        tc.store('jasmine.lastFailedIds', [1, 3, 5]);
-        tc.store('jasmine.lastCount', 5);
-        jasmineEnv.nextSpecId_ = 5;
-        jasmineEnv.exclusive_ = 1;
-
-        start();
-        expect(jasmineEnv.specFilter).toBe(originalSpecFilter);
-      });
     });
   });
 
