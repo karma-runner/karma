62c3c6790659f8f82f8a2ca5646aa424eeb28842
Vojta Jina <vojta.jina@gmail.com>
2013-03-02 16:47:10 -0800

    fix(coverage): always send a result object
    This is just a fix for stable, in the master I will change the implementation,
    so that the "testacular" is responsible for sending the coverage results,
    rather than individual adapters.
    
    Closes #365

--- static/testacular.src.js
+++ static/testacular.src.js
@@ -90,7 +90,7 @@ var Testacular = function(socket, context, navigator, location) {
   };
 
   this.complete = function(result) {
-    socket.emit('complete', result);
+    socket.emit('complete', result || {});
     clearContext();
   };
 
