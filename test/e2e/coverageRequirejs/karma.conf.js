4b9533bb5a73bf241e8b40ced922561500f99524
Vojta Jina <vojta.jina@gmail.com>
2013-03-29 16:31:19 -0700

    test(e2e): remove **/ prefix from coverage
    As it should not be required anymore...

--- test/e2e/coverageRequirejs/karma.conf.js
+++ test/e2e/coverageRequirejs/karma.conf.js
@@ -12,7 +12,7 @@ singleRun = false;
 reporters = ['progress', 'coverage'];
 
 preprocessors = {
-  '**/coverageRequirejs/dependency.js': 'coverage'
+  'dependency.js': 'coverage'
 };
 
 coverageReporter = {
