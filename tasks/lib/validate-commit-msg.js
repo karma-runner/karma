65c3623799d0dcf462b2c0bcaa5cafc335043b50
Iristyle <Iristyle@github>
2013-03-29 13:29:50 -0400

    chore: validate-commit-msg, further restrictions
     - Ensure 2nd line empty
     - List allowed type names on bad commit msg
     - Configurable first line length vs other line length
     - Allow lines > 72 if they're http links

--- tasks/lib/validate-commit-msg.js
+++ tasks/lib/validate-commit-msg.js
@@ -5,17 +5,22 @@
  * See https://docs.google.com/document/d/1rk04jEuGfk9kYzfqCuOlPTSJw3hEDZJTBN5E5f1SALo/edit
  *
  * Installation:
- * >> cd <angular-repo>
- * >> ln -s ../../validate-commit-msg.js .git/hooks/commit-msg
+ * >> cd <repo>
+ * >> Windows
+ * >> fsutil hardlink create .\.git\hooks\commit-msg .\tasks\lib\validate-commit-msg.js
+ * >> Non-Windows
+ * >> ln -s .tasks/lib/validate-commit-msg.js .git/hooks/commit-msg
  */
 var fs = require('fs');
 var util = require('util');
 var path = require('path');
 
 
+var MAX_FIRST_LINE_LENGTH = 50;
+var ALLOW_EMPTY_SECOND_LINE = false;
 var MAX_LENGTH = 70;
 var PATTERN = /^(\w*)(\(([\w\$\.\-\*]*)\))?\: (.*)$/;
-var IGNORED = /^(WIP\:|Merge pull request)/;
+var IGNORED = /^(WIP\:|Merge pull request|http(s)?\:\/\/)/;
 var TYPES = {
   feat: true,
   fix: true,
@@ -26,6 +31,12 @@ var TYPES = {
   chore: true
 };
 
+var TYPE_NAMES = [];
+for (var typeName in TYPES) {
+  if (TYPES.hasOwnProperty(typeName)) {
+      TYPE_NAMES.push(typeName);
+  }
+}
 
 var error = function() {
   // gitx does not display it
@@ -33,31 +44,48 @@ var error = function() {
   console.error('INVALID COMMIT MSG: ' + util.format.apply(null, arguments));
 };
 
+var validateLength = function(message, length, index) {
+  if (message.length > length) {
+      error('line %d is longer than %d characters !', index + 1, length);
+      return false;
+  }
+  return true;
+};
 
-var validateMessage = function(message) {
+var validateMessage = function(message, index) {
   var isValid = true;
+  var firstLine = index === 0,
+    secondLine = index === 1;
 
   if (IGNORED.test(message)) {
     console.log('Commit message validation ignored.');
     return true;
   }
 
-  if (message.length > MAX_LENGTH) {
-    error('is longer than %d characters !', MAX_LENGTH);
-    isValid = false;
+  if (secondLine && !ALLOW_EMPTY_SECOND_LINE && (message.length > 0)) {
+    error('second line must be empty!');
+    return false;
   }
+
+  var maxLength = firstLine ? MAX_FIRST_LINE_LENGTH : MAX_LENGTH;
+  isValid = validateLength(message, maxLength, index);
+  if (index > 0) {
+    return isValid;
+  }
+
+  var validTypes = 'valid <type> values are: ' + TYPE_NAMES.join(', ');
 
   var match = PATTERN.exec(message);
 
   if (!match) {
-    error('does not match "<type>(<scope>): <subject>" !');
+    error('does not match "<type>(<scope>): <subject>" !\n' + validTypes);
     return false;
   }
 
   var type = match[1];
 
   if (!TYPES.hasOwnProperty(type)) {
-    error('"%s" is not allowed type !', type);
+    error('"%s" is not allowed type !\n' + validTypes, type);
     return false;
   }
 
@@ -73,11 +101,10 @@ var validateMessage = function(message) {
   return isValid;
 };
 
-
-var firstLineFromBuffer = function(buffer) {
-  return buffer.toString().split('\n').shift();
+var linesFromBuffer = function(buffer) {
+  return buffer.toString()
+    .split('\n');
 };
-
 
 // publish for testing
 exports.validateMessage = validateMessage;
@@ -88,10 +115,17 @@ if (process.env.GIT_DIR) {
   var incorrectLogFile = path.dirname(commitMsgFile) + '/logs/incorrect-commit-msgs';
 
   fs.readFile(commitMsgFile, function(err, buffer) {
-    var msg = firstLineFromBuffer(buffer);
+    var msg = linesFromBuffer(buffer);
 
-    if (!validateMessage(msg)) {
-      fs.appendFile(incorrectLogFile, msg + '\n', function() {
+    var errorMsg = '';
+    for (var i = 0; i < msg.length; ++i) {
+      if (!validateMessage(msg[i], i)) {
+        errorMsg += (msg + '\n');
+      }
+    }
+
+    if (errorMsg !== '') {
+      fs.appendFile(incorrectLogFile, errorMsg, function() {
         process.exit(1);
       });
     } else {
