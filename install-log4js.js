39f9d6675fa0740a10f4416dde85854f58480ea1
Vojta Jina <vojta.jina@gmail.com>
2013-04-01 22:28:35 -0700

    chore(deps): improve the hack for installing log4js
    I'm really getting tired of this crap...

--- install-log4js.js
+++ install-log4js.js
@@ -3,5 +3,7 @@
 var spawn = require('child_process').spawn;
 
 if (/0\.10/.test(process.versions.node)) {
-  spawn('npm', ['install', 'log4js@0.6.1'], {stdio: 'inherit'});
+  spawn('npm', ['install', 'log4js@0.6.2'], {stdio: 'inherit'});
+} else if (/0\.8/.test(process.versions.node)) {
+  spawn('npm', ['install', 'log4js@0.5.8'], {stdio: 'inherit'});
 }
