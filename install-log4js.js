0704cba3c236dffd696e3f3e52ae4b670a6b7682
Vojta Jina <vojta.jina@gmail.com>
2013-04-02 15:23:59 -0700

    chore(deps): fix the hack for log4js to work on Windows
    This is unbelievable.

--- install-log4js.js
+++ install-log4js.js
@@ -1,9 +1,20 @@
+#!/usr/bin/env node
+
 // workaround hack for https://github.com/isaacs/npm/issues/3305
 
-var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
+var version;
 
 if (/0\.10/.test(process.versions.node)) {
-  spawn('npm', ['install', 'log4js@0.6.2'], {stdio: 'inherit'});
+  version = '0.6.2';
 } else if (/0\.8/.test(process.versions.node)) {
-  spawn('npm', ['install', 'log4js@0.5.8'], {stdio: 'inherit'});
+  version = '0.5.8';
+}
+
+if (version) {
+  var cp = exec('npm install log4js@' + version);
+
+  // because stdin: 'inherit' does not work on Windows
+  cp.stdout.pipe(process.stdout);
+  cp.stderr.pipe(process.stderr);
 }
