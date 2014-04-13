a3a7556baf811979c8724224e2eac9e2ce2465cc
Vojta Jina <vojta.jina@gmail.com>
2014-04-12 22:37:30 -0700

    chore(scripts): add merge_pr

---
+++ scripts/merge_pr.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Take a PR from Github, merge it into master/stable and push that into presubmit-master/stable
+# so that presubmit will merge it into master/stable if all the tests pass.
+
+
+PR_NUMBER=$1
+ORIGINAL_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+# Stash local changes first.
+# TODO(vojta): Only check for modifications?
+if [ "$(git status -s)" ]; then
+  git stash
+  STASHED=true
+fi
+
+if [ $PR_NUMBER ]; then
+  PR_BRANCH_CREATED=true
+  PR_BRANCH="pr-$PR_NUMBER"
+  git fetch upstream pull/$PR_NUMBER/head:$PR_BRANCH
+  git checkout $PR_BRANCH
+else
+  echo "No PR number provided, merging the current branch."
+  PR_BRANCH=$ORIGINAL_BRANCH
+fi
+
+# Make sure we have all the latest bits.
+git fetch upstream master
+git fetch upstream stable
+
+# Determine whether merging to master/stable.
+COMMON_PARENT=$(git merge-base $PR_BRANCH master)
+if git merge-base --is-ancestor $COMMON_PARENT stable; then
+  MERGE_INTO="stable"
+else
+  MERGE_INTO="master"
+fi
+
+echo "Merging into $MERGE_INTO..."
+MERGING_BRANCH="presubmit-$MERGE_INTO-$PR_BRANCH"
+git checkout -b $MERGING_BRANCH upstream/$MERGE_INTO
+git merge $PR_BRANCH
+git push upstream $MERGING_BRANCH
+
+# Revert the original state of the workspace.
+git checkout $ORIGINAL_BRANCH
+if [ "$STASHED" ]; then
+  git stash pop
+fi
+
+git branch -D $MERGING_BRANCH
+if [ "$PR_BRANCH_CREATED" ]; then
+  git branch -D $PR_BRANCH
+fi
