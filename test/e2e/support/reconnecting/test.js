d662f934bf644a8188d663929f8c35cd6d54c2f2
dignifiedquire <dignifiedquire@gmail.com>
2015-01-04 22:48:03 +0100

    test(e2e): Upgrade to Jasmine 2.

--- test/e2e/support/reconnecting/test.js
+++ test/e2e/support/reconnecting/test.js
@@ -1,16 +1,5 @@
 describe('plus', function() {
 
-  var breath = function() {
-    var finished = false;
-    setTimeout(function() {
-      finished = true;
-    }, 0)
-
-    waitsFor(function() {
-      return finished;
-    });
-  };
-
   // super hacky way to get the actual socket to manipulate it...
   var socket = function() {
     var location = window.parent.location;
@@ -21,23 +10,25 @@ describe('plus', function() {
     expect(1).toBe(1);
   });
 
-  it('should disconnect', function() {
+  it('should disconnect', function(done) {
+    console.log(2);
     expect(2).toBe(2);
     socket().disconnect();
 
-    breath();
+    done();
   });
 
   it('should work', function() {
     expect(plus(1, 2)).toBe(3);
   });
 
-  it('should re-connect', function() {
+  it('should re-connect', function(done) {
+    console.log(4);
     expect(4).toBe(4);
     socket().reconnect();
     // window.parent.socket.socket.connect();
 
-    breath();
+    done();
   });
 
   it('should work', function() {
