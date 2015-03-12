d8521ef4adc0d2878a67f076d5d2042d8a05bd5f
Jonathan Freeman <freethejazz@gmail.com>
2015-03-11 19:30:40 -0500

    fix(init): fix test-main.(js/coffee) generation
    The `pathToModule` function sets a project up such that your spec
    directory must be within your src directory. This approach directly
    contradicts the example describing how to use RequireJS with Karma in
    the documentation.
    
    Fixes #1120, fixes #896

--- requirejs.config.tpl.js
+++ requirejs.config.tpl.js
@@ -1,14 +1,10 @@
 var allTestFiles = [];
 var TEST_REGEXP = /(spec|test)\.js$/i;
 
-var pathToModule = function(path) {
-  return path.replace(/^\/base\//, '').replace(/\.js$/, '');
-};
-
 Object.keys(window.__karma__.files).forEach(function(file) {
   if (TEST_REGEXP.test(file)) {
     // Normalize paths to RequireJS module names.
-    allTestFiles.push(pathToModule(file));
+    allTestFiles.push(file);
   }
 });
 
