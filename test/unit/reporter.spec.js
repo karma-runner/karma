a216cc67006ba9e0978783cfb4bf52dc65fd4c71
Levi Thomason <me@levithomason.com>
2016-08-16 10:26:28 -0700

    test(reporter): add config.formatError tests

--- test/unit/reporter.spec.js
+++ test/unit/reporter.spec.js
@@ -2,6 +2,7 @@ import {EventEmitter} from 'events'
 import {loadFile} from 'mocks'
 import path from 'path'
 import _ from 'lodash'
+import sinon from 'sinon'
 
 import File from '../../lib/file'
 
@@ -15,10 +16,43 @@ describe('reporter', () => {
   describe('formatError', () => {
     var emitter
     var formatError = emitter = null
+    var sandbox
 
     beforeEach(() => {
       emitter = new EventEmitter()
       formatError = m.createErrorFormatter({ basePath: '' }, emitter)
+      sandbox = sinon.sandbox.create()
+    })
+
+    it('should call config.formatError if defined', () => {
+      var spy = sandbox.spy()
+      formatError = m.createErrorFormatter({ basePath: '', formatError: spy }, emitter)
+      formatError()
+
+      expect(spy).to.have.been.calledOnce
+    })
+
+    it('should not call config.formatError if not defined', () => {
+      var spy = sandbox.spy()
+      formatError()
+
+      expect(spy).not.to.have.been.calledOnce
+    })
+
+    it('should pass the error message as the first config.formatError argument', () => {
+      var ERROR = 'foo bar'
+      var spy = sandbox.spy()
+      formatError = m.createErrorFormatter({ basePath: '', formatError: spy }, emitter)
+      formatError(ERROR)
+
+      expect(spy.firstCall.args[0]).to.equal(ERROR)
+    })
+
+    it('should display the error returned by config.formatError', () => {
+      var formattedError = 'A new error'
+      formatError = m.createErrorFormatter({ basePath: '', formatError: () => formattedError }, emitter)
+
+      expect(formatError('Something', '\t')).to.equal(formattedError + '\n')
     })
 
     it('should indent', () => {
