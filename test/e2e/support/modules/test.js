2158d938f7f59d225bca034df9a8f567150f6399
Wesley Cho <wesley.cho@gmail.com>
2017-05-04 08:21:13 -0700

    feat(config): add support for ES modules
    - Add support for ES modules via `esModule` property in file config object

---
+++ test/e2e/support/modules/test.js
@@ -0,0 +1,10 @@
+/* globals plus */
+describe('plus', function () {
+  it('should pass', function () {
+    expect(true).toBe(true)
+  })
+
+  it('should work', function () {
+    expect(plus(1, 2)).toBe(3)
+  })
+})
