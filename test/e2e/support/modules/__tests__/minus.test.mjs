8addb9b634b9ccfe9e5f475c8339c78113383751
Brenton Simpson <appsforartists@google.com>
2017-05-04 16:18:53 -0700

    test(modules): ensure relative paths work correctly

--- test/e2e/support/modules/__tests__/minus.test.mjs
+++ test/e2e/support/modules/__tests__/minus.test.mjs
@@ -1,10 +1,11 @@
-/* globals plus */
-describe('plus', function () {
+import { minus } from '../minus.mjs'
+
+describe('minus', function () {
   it('should pass', function () {
     expect(true).toBe(true)
   })
 
   it('should work', function () {
-    expect(plus(1, 2)).toBe(3)
+    expect(minus(3, 2)).toBe(1)
   })
 })
