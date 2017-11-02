65a5117ff18f6fee0b099c2ac315387a08e60376
johnjbarton <johnjbarton@johnjbarton.com>
2017-11-02 09:31:25 -0700

    fix(logging): Upgrade to log4js 2.x API.
    Support log4js 2.x as well as 1.x for configuration.
    
    Fixes #2858

--- test/unit/logger.spec.js
+++ test/unit/logger.spec.js
@@ -3,19 +3,43 @@ import path from 'path'
 
 describe('logger', () => {
   var m
+  let configuration
 
   beforeEach(() => {
-    m = loadFile(path.join(__dirname, '/../../lib/logger.js'))
+    const mockLog4Js = {
+      configure: function (config) {
+        configuration = config
+      }
+    }
+    m = loadFile(path.join(__dirname, '/../../lib/logger.js'), {'log4js': mockLog4Js})
   })
 
   describe('setup', () => {
-    it('should allow for configuration via setup() using an array', () => {
+    it('should allow for configuration via setup() using an array for back-compat', () => {
       m.setup('INFO', true, [{
         type: 'file',
         filename: 'test/unit/test.log'
       }])
-
-      expect(m.log4js.appenders).to.have.keys(['console', 'file', 'stdout'])
+      expect(configuration).to.have.keys(['appenders', 'categories'])
+      expect(configuration.appenders).to.have.keys(['0'])
+      expect(configuration.appenders['0'].type).to.equal('file')
+      expect(configuration.categories).to.have.keys(['default'])
+      expect(configuration.categories.default.appenders).to.have.keys(['0'])
+      expect(configuration.categories.default.level).to.equal('INFO')
+    })
+    it('should allow setup() using log4js v2 object', () => {
+      m.setup('WARN', true, {
+        'fileAppender': {
+          type: 'file',
+          filename: 'test/unit/test.log'
+        }
+      })
+      expect(configuration).to.have.keys(['appenders', 'categories'])
+      expect(configuration.appenders).to.have.keys(['fileAppender'])
+      expect(configuration.appenders['fileAppender'].type).to.equal('file')
+      expect(configuration.categories).to.have.keys(['default'])
+      expect(configuration.categories.default.appenders[0]).to.equal('fileAppender')
+      expect(configuration.categories.default.level).to.equal('WARN')
     })
   })
 })
