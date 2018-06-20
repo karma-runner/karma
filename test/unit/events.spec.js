f7852f6d5e04a89d50c13b8f72f27a2a08bb6c00
johnjbarton <johnjbarton@johnjbarton.com>
2018-06-19 18:44:50 -0700

    fix(events): bind emitters with for..in.
    Fixes #3057

--- test/unit/events.spec.js
+++ test/unit/events.spec.js
@@ -20,34 +20,48 @@ describe('events', () => {
       var object = null
 
       beforeEach(() => {
-        object = sinon.stub({
+        // Note: es6 class instances have non-enumerable prototype properties.
+        function FB () {};
+        FB.prototype = {
+          onPrototypeBar () {}
+        }
+        object = new FB()
+        Object.assign(object, {
           onFoo: () => {},
           onFooBar: () => {},
-          foo: () => {},
-          bar: () => {}
+          foo: () => {}
         })
+
         emitter.bind(object)
       })
 
       it('should register all "on" methods to events', () => {
+        sinon.spy(object, 'onFoo')
         emitter.emit('foo')
         expect(object.onFoo).to.have.been.called
 
+        sinon.spy(object, 'onFooBar')
         emitter.emit('foo_bar')
         expect(object.onFooBar).to.have.been.called
 
+        sinon.spy(object, 'onPrototypeBar')
+        emitter.emit('prototype_bar')
+        expect(object.onPrototypeBar).to.have.been.called
+
+        sinon.spy(object, 'foo')
         expect(object.foo).not.to.have.been.called
-        expect(object.bar).not.to.have.been.called
       })
 
       it('should bind methods to the owner object', () => {
+        sinon.spy(object, 'foo')
+        sinon.spy(object, 'onFoo')
+        sinon.spy(object, 'onFooBar')
         emitter.emit('foo')
         emitter.emit('foo_bar')
 
         expect(object.onFoo).to.have.always.been.calledOn(object)
         expect(object.onFooBar).to.have.always.been.calledOn(object)
         expect(object.foo).not.to.have.been.called
-        expect(object.bar).not.to.have.been.called
       })
     })
 
