ad777f066e50eb7e830dd0d30c60625f76753cd9
johnjbarton <johnjbarton@johnjbarton.com>
2020-02-14 11:53:35 -0800

    chore(travis): activate semantic-release on node-12 test success (#3423)

---
+++ release.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  // Add logging for releases until we are fully confident of the release solution.
+  debug: true,
+  branch: 'master',
+  verifyConditions: [
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/github'
+  ],
+  prepare: [
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/git'
+  ],
+  publish: [
+    '@semantic-release/npm',
+    '@semantic-release/github'
+  ],
+  success: [
+    '@semantic-release/github'
+  ]
+}
