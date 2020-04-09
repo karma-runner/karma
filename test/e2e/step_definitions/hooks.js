17a8d4241b77c003a3341a546a3d1a14810843c7
johnjbarton <johnjbarton@johnjbarton.com>
2020-04-09 16:01:02 -0700

    fix(ci): stop the proxy before killing the child, handle errors
    This should fix #3464, hopefully. Since the test is flaky and only on Travis
    we can't be sure until we try for awhile.

--- test/e2e/step_definitions/hooks.js
+++ test/e2e/step_definitions/hooks.js
@@ -4,12 +4,13 @@ cucumber.defineSupportCode((a) => {
   a.After(function (scenario, callback) {
     const running = this.child != null && typeof this.child.kill === 'function'
 
-    if (running) {
-      this.child.kill()
-      this.child = null
-    }
-
     // stop the proxy if it was started
-    this.proxy.stop(callback)
+    this.proxy.stop(() => {
+      if (running) {
+        this.child.kill()
+        this.child = null
+      }
+      callback()
+    })
   })
 })
