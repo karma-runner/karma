9d661920405336c40af8edea7dddd6e2b84c68f2
Yaroslav Admin <devoto13@gmail.com>
2020-05-18 17:56:21 +0200

    chore: increase step timeout to prevent flakiness (#3518)
    Also added a custom error logic, so actual output is printed if condition times out.
    
    Example https://travis-ci.org/github/karma-runner/karma/jobs/688421469#L643

--- test/e2e/step_definitions/utils.js
+++ test/e2e/step_definitions/utils.js
@@ -2,14 +2,18 @@ const { promisify } = require('util')
 
 const sleep = promisify(setTimeout)
 
-module.exports.waitForCondition = async (evaluateCondition, timeout = 1000) => {
+module.exports.waitForCondition = async (evaluateCondition, timeout = 1000, customError = null) => {
   let remainingTime = timeout
   while (!evaluateCondition()) {
     if (remainingTime > 0) {
       await sleep(50)
       remainingTime -= 50
     } else {
-      throw new Error(`Condition not fulfilled, waited ${timeout}ms`)
+      if (customError != null) {
+        throw customError()
+      } else {
+        throw new Error(`Condition not fulfilled, waited ${timeout}ms`)
+      }
     }
   }
 }
