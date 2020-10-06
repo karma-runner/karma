05dc28801627e3ce7054ae548046714dc2cf7a5e
johnjbarton <johnjbarton@johnjbarton.com>
2020-10-06 09:50:36 -0700

    fix(context): do not error when karma is navigating (#3565)
    Change the flag name to karmaNavigating and set it along all paths where
    karma deliberately navigates. Other paths must be wrong.
    
    Fixes #3560

--- client/karma.js
+++ client/karma.js
@@ -4,7 +4,7 @@ var util = require('../common/util')
 
 function Karma (socket, iframe, opener, navigator, location, document) {
   var startEmitted = false
-  var reloadingContext = false
+  var karmaNavigating = false
   var self = this
   var queryParams = util.parseQueryParams(location.search)
   var browserId = queryParams.id || util.generateId('manual-')
@@ -80,6 +80,7 @@ function Karma (socket, iframe, opener, navigator, location, document) {
 
   var childWindow = null
   function navigateContextTo (url) {
+    karmaNavigating = true
     if (self.config.useIframe === false) {
       // run in new window
       if (self.config.runInParent === false) {
@@ -89,9 +90,11 @@ function Karma (socket, iframe, opener, navigator, location, document) {
           childWindow.close()
         }
         childWindow = opener(url)
+        karmaNavigating = false
       // run context on parent element (client_with_context)
       // using window.__karma__.scriptUrls to get the html element strings and load them dynamically
       } else if (url !== 'about:blank') {
+        karmaNavigating = false
         var loadScript = function (idx) {
           if (idx < window.__karma__.scriptUrls.length) {
             var parser = new DOMParser()
@@ -123,20 +126,19 @@ function Karma (socket, iframe, opener, navigator, location, document) {
     // run in iframe
     } else {
       iframe.src = policy.createURL(url)
+      karmaNavigating = false
     }
   }
 
   this.onbeforeunload = function () {
-    if (!reloadingContext) {
+    if (!karmaNavigating) {
       // TODO(vojta): show what test (with explanation about jasmine.UPDATE_INTERVAL)
       self.error('Some of your tests did a full page reload!')
     }
-    reloadingContext = false
+    karmaNavigating = false
   }
 
   function clearContext () {
-    reloadingContext = true
-
     navigateContextTo('about:blank')
   }
 
