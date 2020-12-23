e80975c3d60f8e87880b12f818f41f4873ef5fec
Yaroslav Admin <devoto13@gmail.com>
2020-12-23 12:54:56 +0200

    chore(build): remove obsolete Grunt tasks
    These tasks are now handled by the semantic-release or other scripts and therefore we can clean them up.

--- gruntfile.js
+++ gruntfile.js
@@ -26,70 +26,12 @@ module.exports = function (grunt) {
           'test/unit/**/*.spec.js'
         ]
       }
-    },
-    'npm-publish': {
-      options: {
-        abortIfDirty: true,
-        tag: 'latest'
-      }
-    },
-    'npm-contributors': {
-      options: {
-        commitMessage: 'chore: update contributors'
-      }
-    },
-    conventionalChangelog: {
-      release: {
-        options: {
-          changelogOpts: {
-            preset: 'angular'
-          }
-        },
-        src: 'CHANGELOG.md'
-      }
-    },
-    conventionalGithubReleaser: {
-      release: {
-        options: {
-          auth: {
-            type: 'oauth',
-            token: process.env.GH_TOKEN
-          },
-          changelogOpts: {
-            preset: 'angular'
-          }
-        }
-      }
-    },
-    bump: {
-      options: {
-        updateConfigs: ['pkg'],
-        commitFiles: [
-          'package.json',
-          'CHANGELOG.md'
-        ],
-        commitMessage: 'chore: release v%VERSION%',
-        prereleaseName: 'rc'
-      }
     }
   })
 
-  grunt.loadNpmTasks('grunt-check-clean')
   grunt.loadTasks('tasks')
   require('load-grunt-tasks')(grunt)
 
   grunt.registerTask('default', ['test'])
   grunt.registerTask('test-appveyor', ['test:unit', 'test:client'])
-
-  grunt.registerTask('release', 'Build, bump and publish to NPM.', function (type) {
-    grunt.task.run([
-      'check_clean',
-      'npm-contributors',
-      'bump:' + (type || 'patch') + ':bump-only',
-      'conventionalChangelog',
-      'bump-commit',
-      'conventionalGithubReleaser',
-      'npm-publish'
-    ])
-  })
 }
