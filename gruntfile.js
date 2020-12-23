8646b34dfba53126974df0ba9447c03961bd3966
Yaroslav Admin <devoto13@gmail.com>
2020-12-23 17:58:24 +0200

    chore(test): run client tests without grunt wrapper

--- gruntfile.js
+++ gruntfile.js
@@ -1,18 +1,5 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    pkgFile: 'package.json',
-    files: {
-      server: ['lib/**/*.js'],
-      client: ['client/**/*.js'],
-      common: ['common/**/*.js'],
-      context: ['context/**/*.js'],
-      grunt: ['grunt.js', 'tasks/*.js']
-    },
-    test: {
-      unit: 'mochaTest:unit',
-      client: 'test/client/karma.conf.js'
-    },
     mochaTest: {
       options: {
         reporter: 'dot',
@@ -29,9 +16,7 @@ module.exports = function (grunt) {
     }
   })
 
-  grunt.loadTasks('tasks')
-  require('load-grunt-tasks')(grunt)
+  grunt.loadNpmTasks('grunt-mocha-test')
 
-  grunt.registerTask('default', ['test'])
-  grunt.registerTask('test-appveyor', ['test:unit', 'test:client'])
+  grunt.registerTask('default', ['mochaTest:unit'])
 }
